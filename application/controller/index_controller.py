2283cf90f6c89323fff9d50b741cf73548fd0b02
Gabriel <tamiozzo98@gmail.com>
2021-06-15 16:01:24 -0300

    colocando redirect no adicionar e excluir

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,7 +1,7 @@
 
 
 from application import app
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,url_for
 from application.model.dao.produto_dao import ProdutoDAO
 from application.model.entily.produto import Produto
 
@@ -19,13 +19,14 @@ def adicionar(id):
     for produto in produto_list:
         if produto.get_id() == int(id):#esse id vem como str e temos q transformar em int.
             carrinho_list.append(produto)
+    return redirect(url_for('index'))
     
-    return render_template("index.html",produto_list = produto_list,carrinho_list = carrinho_list)
 
 @app.route("/excluir/<id>")
 def excluir(id):
     for produto in carrinho_list:
         if produto.get_id() == int(id):
             carrinho_list.remove(produto)
-    return render_template("index.html", carrinho_list=carrinho_list, produto_list=produto_list)
+    return redirect(url_for('index'))
+    
     
